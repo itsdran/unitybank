b900fc2c18fb64d86a17cbe20d92d67e95948d21
NamPineda <nampineda2021@plm.edu.ph>
2022-06-07 20:30:58 +0800

    About/Setting CSS -Pineda

---
+++ templates/setting-sample.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <meta http-equiv="refresh" content="">
+    <title>Settings</title>
+</head>
+<body>
+    <?php include ("navbarfixed.php");?>
+    <div class="body">
+        <div class="setting-container">   
+            <div class="setting-content">
+                <h4 style="padding-top: 20px;">Notifications Settings</h4>
+                <p>Select notification you want to receive</p>
+                <hr>
+                <input type="checkbox" class="smoller" checked="">
+                <strong>Security</strong>
+                <p>Control security alert you will be notified.</p>
+                <hr>
+                <input type="checkbox" class="smoller" checked="">
+                <strong>Unusual activity notifications</strong>
+                <p>Donec in quam sed urna bibendum tincidunt quis mollis mauris.</p>
+                <hr>
+
+                <input type="checkbox" class="smoller" checked="">
+                <strong>Unauthorized financial activity</strong>
+                <p>Fusce lacinia elementum eros, sed vulputate urna eleifend nec.</p>
+                    
+                <hr>
+                <h4>System</h4>
+                <p>Please enable system alert you will get.</p>
+                <hr>
+
+                <input type="checkbox" class="smoller" checked="">
+                <strong>Notify me about new features and updates</strong>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <hr>
+
+                <input type="checkbox" class="smoller" checked="">
+                <strong>Notify me by email for latest news</strong>
+                <p>Nulla et tincidunt sapien. Sed eleifend volutpat elementum.</p>
+                <hr>
+
+                <input type="checkbox" class="smoller" checked="">
+                <strong>Notify me about tips on using account</strong>
+                <p>Donec in quam sed urna bibendum tincidunt quis mollis mauris.</p>
+                <hr>
+
+                <input type="checkbox" class="smoller">
+                <strong>General Notification</strong>
+                <p>Donec in quam sed urna bibendum tincidunt quis mollis mauris.</p>
+            </div>                
+        </div> 
+    </div>
+</div>
+</body>
+</html>
