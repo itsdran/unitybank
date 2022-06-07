e1026a15565e3526149a0a18543c2afca5cb47b2
vanallen04 <106895196+vanallen04@users.noreply.github.com>
2022-06-07 00:31:19 -0600

    mga binago ko - vanallen04
    yung css ko may nakalagay na sample.php na css tapos nung una gumana yung light green sa akin pero nung inopen ko ulit yung file hindi na, nasa css yun pacheck na lang

---
+++ dashboard.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Navbar sample</title>
+</head>
+<body>
+    <?php include ("navbarfixed.php");?>
+    <div class="body">
+        <div class="dashboard-body">
+          <div class="dashboard-left">
+            <div class="dashboard-section">
+              <h5>ACCOUNTS</h5>
+              <div class="dashboard-section-content">
+
+                <div id="image">
+        <img src="visa  logo-medium.png" alt="Logo" height="30px" width="60px">
+      </div>
+
+&nbsp;
+<br>
+&nbsp;
+<br>
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+<div class="input">
+      <input type="number" name="account1" place holder="Account1"/>
+    </div>
+
+
+
+              </div>
+
+              <div class="dashboard-section-content">
+                <div id="image">
+        <img src="paypal.png" alt="Logo" height="70px" width="80px">
+      </div>
+
+&nbsp;
+
+
+
+      <div class="input">
+      <input type="number" name="account2" place holder="Account2"/>
+    </div>
+              </div>
+            </div>
+           
+          </div>
+          <div class="dashboard-right">
+            <div class="dashboard-section">
+              <h5>GOALS</h5>
+
+              <div class="dashboard-section-content">
+                 <!-- Content ng GOALS ditu -->
+                
+              </div>
+            </div>
+
+            <div class="dashboard-section">
+              <h5>BRANCH VISIT</h5>
+              <div class="dashboard-section-content">
+
+                  <!-- Content ng BRANCH VISIT ditu -->
+
+              </div>
+            </div>
+
+            <div class="dashboard-section">
+              <h5>OTHER OPTIONS</h5>
+              <div class="dashboard-section-content">
+                <!-- Content ng other options ditu -->
+
+              </div>
+            </div>
+        </div>               
+      </div>
+    </div>
+</body>
+</html>
