31e933a4c446fad0575870f6048cfb2e9db4fecb
vanallen04 <106895196+vanallen04@users.noreply.github.com>
2022-06-07 14:49:44 -0600

    mga binago ko
    inaayos ko na yung sa css ko check niyo nakaupload na din yung mga php tsaka picture

---
+++ pay-bills.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Navbar sample</title>
+</head>
+<body>
+    <?php include ("navbarfixed.php");?>
+    <div class="body">
+        <div class="pay-bills-body">
+        <div class="pay-bills-section">
+            <p class="pay-bills-option">Select Biller</p>
+          </div>
+          <div class="pay-bills-section">
+            <h5>MANAGE</h5>
+            <p class="pay-bills-option">Recent Payments</p>
+            <p class="pay-bills-option">Manage Billers</p>
+            <p class="pay-bills-option">Manage Scheduled Payments</p>
+            <p class="pay-bills-option">FAQs</p>
+          </div>
+        </div>               
+      </div>
+    </div>
+</body>
+</html>
