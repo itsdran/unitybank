d30e50eb5db1c89a4c9ceff0cfc6cde01bc0ca82
vanallen04 <106895196+vanallen04@users.noreply.github.com>
2022-06-12 18:13:58 -0600

    for transfer-money - vanallen04
    pre eto yung sa css for transfer money and php file for transfer money din pre kulang pa yan ng mga href pre

---
+++ transfer-money.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Navbar sample</title>
+</head>
+<body>
+    <?php include ("navbarfixed.php");?>
+    <div class="body">
+        <div class="transfer-money-body">
+          <div class="where-transfer">
+            <h1>Where would you like to transfer?</h1>
+            <div class="where-transfer-options">
+              <a href="">OWN ACCOUNT</a>
+              <a href="">ANOTHER ACCOUNT</a>
+              <a href="">OTHER BANKS</a>
+              <a href="">EON ACCOUNT</a>
+            </div>
+          </div>
+
+          <div class="transfer-money-more-options">
+            <a href="">VIEW HISTORY</a>
+            <a href="">MANAGE RECIPIENTS</a>
+            <a href="">MANAGE TRANSFER RECORDS</a>
+            <a href="">FAQs</a>
+          </div>
+        </div>               
+      </div>
+    </div>
+</body>
+</html>
